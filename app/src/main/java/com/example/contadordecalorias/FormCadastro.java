a352bbd9c66674d63ab319b3aca729007e068877
Leonardo Castro Alves <leo_alves25@yahoo.com.br>
2023-04-02 21:43:07 -0300

    Cadastro usuário

--- app/src/main/java/com/example/contadordecalorias/FormCadastro.java
+++ app/src/main/java/com/example/contadordecalorias/FormCadastro.java
@@ -1,6 +1,5 @@
 package com.example.contadordecalorias;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
@@ -9,18 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.ktx.Firebase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import entities.User;
 
 public class FormCadastro extends AppCompatActivity {
 
-    private EditText edit_name, edit_email, edit_password;
+    private EditText edit_name, edit_email, edit_password, edit_gender, edit_activity, edit_age, edit_height, edit_weight, edit_fat;
     private Button btn_register;
-    String[] messages = {"Fill in all fields", "Registration completed successfully"};
+    String[] messages = {"Fill in all fields", "Registration completed successfully", "Error to register user"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +27,6 @@ public class FormCadastro extends AppCompatActivity {
 
         getSupportActionBar().hide();
         startComponents();
-
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String name = edit_name.getText().toString();
-                String email = edit_email.getText().toString();
-                String password = edit_password.getText().toString();
-
-                //Condicional para exibir uma menssagem caso o usuário tente se cadastrar sem preencher todos os campos
-                if(name.isEmpty() == true || email.isEmpty() == true || password.isEmpty() == true) {
-
-                    Snackbar snackbar = Snackbar.make(v, messages[0], Snackbar.LENGTH_SHORT);
-                    snackbar.setBackgroundTint(Color.WHITE);
-                    snackbar.setTextColor(Color.BLACK);
-                    snackbar.show();
-
-                } else {
-
-                }
-
-            }
-        });
     }
 
     //método para conectar os objetos a uma referencia
@@ -59,25 +34,67 @@ public class FormCadastro extends AppCompatActivity {
         edit_name = findViewById(R.id.name_register);
         edit_email = findViewById(R.id.email_define);
         edit_password = findViewById(R.id.password_define);
+        edit_age = findViewById(R.id.age);
+        edit_weight = findViewById(R.id.weight);
+        edit_height = findViewById(R.id.height);
+        edit_fat = findViewById(R.id.body_fat);
+        edit_gender = findViewById(R.id.gender);
+        edit_activity = findViewById(R.id.activity);
         btn_register = findViewById(R.id.register_button);
     }
 
     public void registerUser(View v) {
 
+        String name = edit_name.getText().toString();
         String email = edit_email.getText().toString();
         String password = edit_password.getText().toString();
+        String gender = edit_gender.getText().toString();
+        String activity = edit_activity.getText().toString();
 
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+        int age = Integer.parseInt(edit_age.getText().toString());
 
-                if(task.isSuccessful() == true) {
-                    Snackbar snackbar = Snackbar.make(v, messages[1], Snackbar.LENGTH_SHORT);
+        float height = Float.parseFloat(edit_height.getText().toString());
+        float weight = Float.parseFloat(edit_weight.getText().toString());
+        float fat = Float.parseFloat(edit_fat.getText().toString());
+
+        //Condicional para exibir uma menssagem caso o usuário tente se cadastrar sem preencher todos os campos
+        if(name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+
+            Snackbar snackbar = Snackbar.make(v, messages[0], Snackbar.LENGTH_SHORT);
+            snackbar.setBackgroundTint(Color.WHITE);
+            snackbar.setTextColor(Color.BLACK);
+            snackbar.show();
+
+        } else {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    // Cria um documento com as informações adicionais do usuário
+
+                    User user = new User(name, age,  height,  weight,  fat,  activity, gender, email);
+
+                    String uid = task.getResult().getUser().getUid();
+                    FirebaseFirestore.getInstance().collection("users").document(uid)
+                            .set(user.toMap())
+                            .addOnSuccessListener(aVoid -> {
+                                Snackbar snackbar = Snackbar.make(v, messages[1], Snackbar.LENGTH_SHORT);
+                                snackbar.setBackgroundTint(Color.WHITE);
+                                snackbar.setTextColor(Color.BLACK);
+                                snackbar.show();
+                            })
+                            .addOnFailureListener(e -> {
+                                Snackbar snackbar = Snackbar.make(v, messages[2], Snackbar.LENGTH_SHORT);
+                                snackbar.setBackgroundTint(Color.WHITE);
+                                snackbar.setTextColor(Color.BLACK);
+                                snackbar.show();
+                            });
+                } else {
+                    // Falha ao criar uma conta de usuário
+                    Snackbar snackbar = Snackbar.make(v, messages[3], Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
                 }
-            }
-        });
+            });
+        }
     }
 }
---
+++ app/src/main/java/entities/User.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+
+    public String name;
+    public int age;
+    public double height;
+    public double weight;
+    public double fat;
+    public String activity;
+    public String gender;
+    public String email;
+    public boolean adm;
+
+
+    public User(String name, int age, double height, double weight, double fat, String activity, String gender, String email){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.fat = fat;
+        this.activity = activity;
+        this.gender = gender;
+        this.email = email;
+        this.adm = false;
+    }
+
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("age", age);
+        result.put("height", height);
+        result.put("weight", weight);
+        result.put("fat", fat);
+        result.put("activity", activity);
+        result.put("gender", gender);
+        result.put("email", email);
+        result.put("adm", adm);
+        return result;
+    }
+
+}
