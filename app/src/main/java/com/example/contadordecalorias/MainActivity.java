7e81c72a4b9d99c8a223e32a0f04a591f621b917
Leonardo Batista <LeonardoMTBat@gmail.com>
2023-04-06 20:26:25 -0300

    Barra de navegação inferior
    Adição do menu inferior integrado a três fragmentos base de tela (As três principais do app)

--- app/src/main/java/com/example/contadordecalorias/MainActivity.java
+++ app/src/main/java/com/example/contadordecalorias/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.contadordecalorias;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.contadordecalorias.databinding.ActivityMainBinding;
 import com.google.firebase.ktx.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
+    private NavHostFragment navHostFragment;
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        initNavigation();
         //Database-------------------------------------
         DBAdapter db = new DBAdapter(this);
         db.open();
         db.close();
 
         Toast.makeText(this, "Database funciona, e a comida foi criada!!!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void initNavigation(){
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation,navController);
     }
 }
